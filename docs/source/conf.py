d4d944801fafacac57d1b447a4915a45785c1aa9
jm130 <50483084+jm130@users.noreply.github.com>
2023-04-27 17:03:09 +0200

    Update conf.py
    edited project details

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DMOG docs'
+copyright = '2023, Jose M Menendez'
+author = 'Jose M Menendez'
 
 release = '0.1'
 version = '0.1.0'
