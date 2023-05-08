abd60f8934d6a76745e78db745c7c4fa751149ff
jm130 <50483084+jm130@users.noreply.github.com>
2023-05-08 15:46:03 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'DMOG docs'
 copyright = '2023, Jose M Menendez'
 author = 'Jose M Menendez'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
